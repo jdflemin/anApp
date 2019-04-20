3a4ff80bacf05404c404851eec9b893dda43c8aa
Derek Loche <derekmloche@gmail.com>
2019-04-20 01:44:01 -0700

    oops

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ write user input criteria
 // Enter number of years to save 
 // Enter lump sum of savings
 // Enter interest rate in whole percents (ie 1 for 1%)
+
+console.log("Kill Me Plz");
+
+function pertCalc(principle, years, interestRate) {
+
+    var convInterest = interestRate / 100;
+    var total = principle * Math.exp(convInterest * years);
+    return total;
+}
+console.log(pertCalc(100, 2, 5));

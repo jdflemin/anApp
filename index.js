357c800a6e9f917c9493b9abdca64456407ce61c
jdflemin <jdfleming91@gmail.com>
2019-04-20 01:50:54 -0700

    Jf user input (#1)
    * user input
    
    * fat arrows
    
    * made comments
    
    * spacing

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
 #!/usr/bin/env node
 
-// DEREK
-/*
-write function(s) to hand PERT math
- */
+// readline from node
+const readline = require('readline');
 
-// Justin
-/*
-write user input criteria
-*/
+// sets up for user input
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-// Enter number of years to save 
-// Enter lump sum of savings
-// Enter interest rate in whole percents (ie 1 for 1%)
+// main method
+let getUserInput = async () => {
+	// get user input
+	let years = await genericQuestion('How many years do you want to save this money for? ');	
+	let money = await genericQuestion(`How much money are you saving for ${years} years? `);
+	let rate = await genericQuestion(`What rate are you expecting to get on $${money} for ${years}? `);
+	
+	console.log(pertCalc(money, years, rate));
+	rl.close()
+}
 
-console.log("Kill Me Plz");
+// generic method for asking questions and returning the input
+let genericQuestion = (question) => {
+	return new Promise(res => {
+		rl.question(question, (input) => {
+			res(input);
+		});
+	});
+}
 
 function pertCalc(principle, years, interestRate) {
-
-    var convInterest = interestRate / 100;
-    var total = principle * Math.exp(convInterest * years);
-    return total;
+	var convInterest = interestRate / 100;
+	var total = principle * Math.exp(convInterest * years);
+	return total;
 }
-console.log(pertCalc(100, 2, 5));
+
+// lets get it started
+getUserInput();
+

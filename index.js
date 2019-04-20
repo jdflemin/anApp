2352d139ccdf30558397de87a091846aa84a64c0
jdflemin <jfleming@radialspark.com>
2019-04-20 00:21:02 -0700

    comments

---
+++ index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+// DEREK
+/*
+write function(s) to hand PERT math
+ */
+
+// Justin
+/*
+write user input criteria
+*/
+
+// Enter number of years to save 
+// Enter lump sum of savings
+// Enter interest rate in whole percents (ie 1 for 1%)
